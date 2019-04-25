6b8abc23350aa2ab5506dd35ff996bc743ea0ade
oivanyts <oivanyts@student.unit.ua>
2019-04-25 05:15:04 -0700

    testted

--- src/vm/load_from_file.c
+++ src/vm/load_from_file.c
@@ -6,27 +6,35 @@
 /*   By: oivanyts <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/24 08:55:42 by oivanyts          #+#    #+#             */
-/*   Updated: 2019/04/25 04:51:16 by oivanyts         ###   ########.fr       */
+/*   Updated: 2019/04/25 05:13:31 by oivanyts         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vm.h"
 #include "op.h"
 
-bool	load_from_file(char	*filename)
+void	output_binary(unsigned char *bitecode, size_t size)
 {
-	int	fd, rd, i = 0;
-	unsigned char bitecode[MEM_SIZE];
+	size_t i;
 
-	if (!(fd = open(filename, O_RDONLY)))
-		return (false);
-	rd = read(fd, bitecode, MEM_SIZE);
-	while (i < rd)
+	i = 0;
+	while (i < size)
 	{
 		ft_printf("%.2x%.2x ", bitecode[i], bitecode[i + 1]);
 		i += 2;
 		if (!(i % 16))
 			ft_printf("\n");
 	}
+}
+
+bool	load_from_file(char	*filename)
+{
+	int				fd;
+	unsigned char	bitecode[MEM_SIZE];
+
+	if (!(fd = open(filename, O_RDONLY)))
+		return (false);
+	output_binary(bitecode, (size_t)read(fd, bitecode, MEM_SIZE));
+
 	return (true);
 }
