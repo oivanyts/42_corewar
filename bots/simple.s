050cc1a1260e8c403ee62672b62b519c0a6eb94a
myaremen <myaremen>
2019-05-30 17:59:41 +0300

    added some bots

---
+++ bots/simple.s
@@ -0,0 +1,8 @@
+.name 		"simple"
+.comment	"..."
+
+main:
+    st r1, 6
+    live %0
+    sub r4, r5, r6
+    zjmp %511
---
+++ bots/virus.s
@@ -0,0 +1,25 @@
+.name 		"virus"
+.comment	"slow(("
+
+live_init:
+    st r1, 34
+
+copy_init:
+    ld %-38, r2 ;src
+    ld %430, r3 ;dst (approximately)
+    ld %68, r4 ;dword counter (approximately)
+    ld %4, r7 ;dword size
+
+live:
+    live %0
+
+copy:
+    ldi r2, r4, r5 ;r5 = dword to copy
+    sti r5, r3, r4 ;copy dword
+    sub r4, r7, r4 ;reduce counter
+    sub r4, r8, r6 ;counter == 0?
+    zjmp %416 ;jump if yes
+
+jmp:
+    ld %0, r10 ; cf = 1
+    zjmp %:live
