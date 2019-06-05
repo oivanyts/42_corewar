1fb2ce8f8ee2207c91a27bd51e00ae0f56df0650
oivanyts <oivanyts@student.unit.ua>
2019-06-05 19:10:37 +0300

    still bad

--- src/vm/opcalls3.c
+++ src/vm/opcalls3.c
@@ -19,8 +19,6 @@ void	f_sub(t_thread *sp, t_memory *p1, t_memory *p2, t_memory *p3)
 	uint32_t sum;
 	t_memory res_memory;
 
-	if (!get_vm(0)->options.visual_ncurses && get_vm(0)->options.o_v_param & 4)
-		output_operation(sp);
 	if (((t_player*)(sp->player))->number == 1)
 	{
 		get_vm(0);
@@ -28,6 +26,8 @@ void	f_sub(t_thread *sp, t_memory *p1, t_memory *p2, t_memory *p3)
 	load_param(sp, p1, 1);
 	load_param(sp, p2, 2);
 	load_param(sp, p3, 3);
+	if (!get_vm(0)->options.visual_ncurses && get_vm(0)->options.o_v_param & 4)
+		output_operation(sp);
 	sum = swap32(swap32(memory_tou32(p1)) - swap32(memory_tou32(p2)));
 	memory_init_number(&res_memory, &sum);
 	memory_memmove(p3, &res_memory);
@@ -36,45 +36,51 @@ void	f_sub(t_thread *sp, t_memory *p1, t_memory *p2, t_memory *p3)
 
 void	f_and(t_thread *sp, t_memory *p1, t_memory *p2, t_memory *p3)
 {
+	load_param(sp, p1, 1);
+	load_param(sp, p2, 2);
+	load_param(sp, p3, 3);
 	if (!get_vm(0)->options.visual_ncurses && get_vm(0)->options.o_v_param & 4)
+	{
 		output_operation(sp);
+	}
 	if (((t_player*)(sp->player))->number == 1)
 	{
 		get_vm(0);
 	}
-	load_param(sp, p1, 1);
-	load_param(sp, p2, 2);
-	load_param(sp, p3, 3);
 	memory_and(p3, p1, p2);
 	sp->cf = memory_iszero(p3);
 }
 
 void	f_or(t_thread *sp, t_memory *p1, t_memory *p2, t_memory *p3)
 {
+	load_param(sp, p1, 1);
+	load_param(sp, p2, 2);
+	load_param(sp, p3, 3);
 	if (!get_vm(0)->options.visual_ncurses && get_vm(0)->options.o_v_param & 4)
+	{
 		output_operation(sp);
+	}
 	if (((t_player*)(sp->player))->number == 1)
 	{
 		get_vm(0);
 	}
-	load_param(sp, p1, 1);
-	load_param(sp, p2, 2);
-	load_param(sp, p3, 3);
 	memory_or(p3, p1, p2);
 	sp->cf = memory_iszero(p3);
 }
 
 void	f_xor(t_thread *sp, t_memory *p1, t_memory *p2, t_memory *p3)
 {
+	load_param(sp, p1, 1);
+	load_param(sp, p2, 2);
+	load_param(sp, p3, 3);
 	if (!get_vm(0)->options.visual_ncurses && get_vm(0)->options.o_v_param & 4)
+	{
 		output_operation(sp);
+	}
 	if (((t_player*)(sp->player))->number == 1)
 	{
 		get_vm(0);
 	}
-	load_param(sp, p1, 1);
-	load_param(sp, p2, 2);
-	load_param(sp, p3, 3);
 	memory_xor(p3, p1, p2);
 	sp->cf = memory_iszero(p3);
 }
